f4018432596f90b164e1c4192a4f2c89b8cf4ee9
nishanthbhat <nishanthbhat.91194@gmail.com>
2016-09-21 23:21:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne = {
+var articles = {
+ 'article-one' : {
     title : 'Article One | Nishanth S Bhat',
     heading : 'Article One',
     date : 'September 20, 2016',
@@ -21,6 +22,28 @@ var articleOne = {
                 <p>
                     This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
                 </p> `
+},
+
+ 'article-two' : {
+    title : 'Article Two | Nishanth S Bhat',
+    heading : 'Article Two',
+    date : 'September 20, 2016',
+    content : `
+                <p>
+                    This is the content for my Second article.
+                </p>`
+ },
+ 
+ 'article-three' : {
+    title : 'Article Three | Nishanth S Bhat',
+    heading : 'Article Three',
+    date : 'September 20, 2016',
+    content : `
+                <p>
+                    This is the content for my Third article.
+                </p>`
+ }
+ 
 };
 
 function createTemplate(data) {
@@ -72,16 +95,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.send(createTemplate(articleOne));
-});
-
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/:articleName', function (req, res) {
+  
+  var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
