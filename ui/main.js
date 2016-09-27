4fb2d112ee8dd69efdb9c64e7f2cb9135e54558f
nishanthbhat <nishanthbhat.91194@gmail.com>
2016-09-27 20:55:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,11 @@
-console.log('Loaded!');
 
-var element = document.getElementById('main-text');
-element.innerHTML = 'New Value';
+var button = document.getElementById('counter');
+var counter = 0;
 
-var img = document.getElementById('madi');
-
-var marginLeft = 0;
-
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function() {
-    var interval = setInterval(moveRight, 50);
+button.onClick = function() {
+    
+    
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 }
